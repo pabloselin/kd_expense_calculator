cb35dba338d4f26a75d681213049be2626112f1a
Pablo Selin Carrasco Armijo <pabloseline@gmail.com>
2022-12-01 11:36:20 +0100

    linting and cleaning unused stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { co2 } from "@tgwf/co2";
-import countries from "./data/countries.json";
+//import countries from "./data/countries.json";
 import expenses_table from "./data/expenses_table.json";
 
 //all style tags
 //const styleTags = document.querySelectorAll("style");
 const stylesheetTags = document.querySelectorAll("link[rel=stylesheet]");
+console.log(stylesheetTags);
 //all img tags
 const imgTags = document.querySelectorAll("img");
 //all script tags
@@ -16,118 +17,87 @@ const htmlLength = document.documentElement.outerHTML.length;
 
 function App() {
   const [calculate, setCalculate] = useState(false);
-  const [ip, setIP] = useState(null);
-  const [intensity, setIntensity] = useState(null);
-  const [pageSize, setPageSize] = useState({
-    images: 0,
-    scripts: 0,
-    styles: 0,
-    html: 0,
-  });
+  // const [ip, setIP] = useState(null);
+  // const [intensity, setIntensity] = useState(null);
+
   const [activity, setActivity] = useState(0);
 
-  const [imageSize, setImageSize] = useState(0);
-  const [scriptSize, setScriptSize] = useState(0);
-  const [styleSize, setStyleSize] = useState(0);
-  const [htmlSize, setHtmlSize] = useState(0);
-  const [totalSize, setTotalSize] = useState(0);
+ 
+  const [totalSize, setTotalSize] = useState(htmlLength);
 
   const [co2e, setCo2e] = useState(0);
-  const [equivalence, setEquivalence] = useState(expenses_table);
+  const equivalence = expenses_table;
   const [isCalculated, setIsCalculated] = useState(false);
-  const [countries, setCountries] = useState(null);
+  //const [countries, setCountries] = useState(null);
 
-  useEffect(() => {
-    if (calculate) {
-      axios.get("https://api.ipify.org?format=json").then((res) => {
-        setIP(res.data.ip);
-      });
-    }
-  }, [calculate]);
+  // useEffect(() => {
+  //   if (calculate) {
+  //     axios.get("https://api.ipify.org?format=json").then((res) => {
+  //       setIP(res.data.ip);
+  //     });
+  //   }
+  // }, [calculate]);
 
-  useEffect(() => {
-    //Carbon intensity
-    if (ip) {
-      axios
-        .get(
-          `https://api.thegreenwebfoundation.org/api/v3/ip-to-co2intensity/${ip}`
-        )
-        .then((res) => {
-          setIntensity(res.data);
-        });
-    }
-  }, [ip]);
+  // useEffect(() => {
+  //   //Carbon intensity
+  //   if (ip) {
+  //     axios
+  //       .get(
+  //         `https://api.thegreenwebfoundation.org/api/v3/ip-to-co2intensity/${ip}`
+  //       )
+  //       .then((res) => {
+  //         setIntensity(res.data);
+  //       });
+  //   }
+  // }, [ip]);
 
   useEffect(() => {
     let min = 0;
     let max = equivalence.length;
     let randomActivity = Math.floor(Math.random() * (max - min)) + min;
     setActivity(randomActivity);
-  }, [isCalculated]);
+  }, [isCalculated, equivalence]);
 
   useEffect(() => {
-    const getFileStats = async (url, category) => {
-      //let fileBlob = await
-      axios
-        .get(url, { responseType: "blob" })
-        .then((res) => {
-          console.log(
-            "getting file stats for: ",
-            `${url} - ${res.data.size / 1000}kb`
-          );
-          console.log("category: ", category);
-          if (category === "images") {
-            setImageSize(imageSize + res.data.size);
-          } else if (category === "scripts") {
-            setScriptSize(scriptSize + res.data.size);
-          } else if (category === "styles") {
-            setStyleSize(styleSize + res.data.size);
-          } else if (category === "html") {
-            setHtmlSize(htmlLength + res.data.size);
-          }
-          //setPageSize({...pageSize,[category]: pageSize[category] + res.data.size});
-        })
-        .catch((err) => {
-          console.log(err);
+    const getFileStatsfromArray = async (urls) => {
+      urls.forEach((url) => {
+        axios.get(url, { responseType: "blob" }).then((res) => {
+          setTotalSize((totalSize) => {
+            console.log(
+              "getting file stats for: ",
+              `${url}: ${res.data.size / 1000}kb / previous ${
+                totalSize / 1000
+              }kb`
+            );
+            return totalSize + res.data.size;
+          });
         });
+      });
     };
 
     if (calculate === true && isCalculated === false) {
-      //calculate size
+      //build array of all file urls
+      let fileUrls = [];
       for (let i = 0; i < imgTags.length; i++) {
-        getFileStats(imgTags[i].src, "images");
-      }
-      for (let i = 0; i < stylesheetTags.length; i++) {
-        if (
-          stylesheetTags[i].hasAttribute("href") ||
-          stylesheetTags[i].hasAttribute("data-href")
-        ) {
-          getFileStats(stylesheetTags[i].src, "styles");
-        }
+        fileUrls.push(imgTags[i].src);
       }
       for (let i = 0; i < scriptTags.length; i++) {
-        getFileStats(scriptTags[i].src, "scripts");
+        fileUrls.push(scriptTags[i].src);
+      }
+      for (let i = 0; i < stylesheetTags.length; i++) {
+        fileUrls.push(stylesheetTags[i].href);
       }
 
-      setPageSize(htmlSize, "html");
+      //get file stats
+      getFileStatsfromArray(fileUrls);
+
+      //set page size
+
+      //setTotalSize(totalSize + htmlSize);
 
       setIsCalculated(true);
     }
-  }, [
-    calculate,
-    pageSize,
-    isCalculated,
-    imageSize,
-    scriptSize,
-    styleSize,
-    htmlSize,
-  ]);
-
-  useEffect(() => {}, [isCalculated]);
-
-  useEffect(() => {
-    setTotalSize(imageSize + scriptSize + styleSize + htmlSize);
-  }, [imageSize, scriptSize, styleSize, htmlSize, totalSize]);
+  }, [calculate, isCalculated, totalSize]);
 
   useEffect(() => {
     if (totalSize) {
@@ -142,24 +112,24 @@ function App() {
         <div className="kd_expense_table">
           <h1>Calculadora de gasto energético digital</h1>
           <span className="kd_detail">
-            (Valores aproximados calculados según ubicación, servidor
-            y dispositivo)
+            (Valores aproximados calculados según ubicación, servidor y
+            dispositivo)
           </span>
 
           {isCalculated && (
             <>
-              <p className="kd_equivalence">
-                Gasto energético: {co2e.toFixed(2)} de co2e (equivalencia de
-                carbono)
+              <p className="kd_activity">
+                El{" "}
+                <strong>
+                  {((co2e * 100) / equivalence[activity].cost).toFixed(4)}%
+                </strong>{" "}
+                de {equivalence[activity].activity}
               </p>
 
-              {activity && equivalence && (
-                <p className="kd_activity">
-                  El{" "}
-                  <strong>{((co2e * 100) / equivalence[activity].cost).toFixed(4)}%</strong>{" "}
-                  de {equivalence[activity].activity}
-                </p>
-              )}
+              <p className="kd_equivalence">
+                Gasto energético: <strong>{co2e.toFixed(2)} de co2e</strong> (equivalencia de
+                carbono)
+              </p>
 
               <p>
                 <button
@@ -172,24 +142,12 @@ function App() {
             </>
           )}
 
-          <p className="kd_ip">IP: {ip}</p>
-          <p>
-            Emisión de carbono por cada kilowatt/hora en tu país:{" "}
-            {intensity && intensity.carbon_intensity}grs
-            <span className="kd_detail">Los valores pueden cambiar según el ranking de emisión energética de tu ubicación</span>
-          </p>
-
           <div className="kd_desglose">
-            <h2>Desglose de transferencia de datos</h2>
             <p>
               Tamaño de todos los elementos de la página:{" "}
-              {totalSize && totalSize / 1000}kb
+              <strong>{totalSize && totalSize / 1000}kb</strong>
             </p>
-            <ul>
-              <li>Imágenes: {imageSize && imageSize / 1000}kb</li>
-              <li>Scripts: {scriptSize && scriptSize / 1000}kb</li>
-              <li>Estilos: {styleSize && styleSize / 1000}kb</li>
-            </ul>
+            
           </div>
         </div>
       ) : (
